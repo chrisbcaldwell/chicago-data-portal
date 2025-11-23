77fae955f7a70b2af5ddf1f115fd428db3820c76
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-11-23 11:14:25 -0800

    updates to taxiupdate.go

--- taxis/taxiupdate.go
+++ taxis/taxiupdate.go
@@ -3,22 +3,43 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"strings"
 
 	_ "github.com/lib/pq"
 )
 
 // connection details
 const (
-	Hostname = "localhost"
-	Port     = 5433
-	Username = "postgres"
-	Password = "pwpwpw"
-	Database = "chicago"
-	Table    = "taxitrips"
+	Hostname      = "localhost"
+	Port          = 5433
+	Username      = "postgres"
+	Password      = "pwpwpw"
+	Database      = "chicago"
+	Table         = "taxitrips"
+	LastUpdateCol = "trip_start_timestamp"
+	URL           = "https://data.cityofchicago.org/resource/wrvz-psew.json"
 )
 
 func main() {
+	// connect to the db
+	db, err := openConnection()
+	if err != nil {
+		fmt.Println("Error connecting to", Database)
+		fmt.Println(err)
+	}
+	defer db.Close()
 
+	// find the newest record already in the database
+	updated := lastUpdate(db)
+
+	// get the JSON of all records newer than what's already loaded in the database
+	request := URL + "?$where=" + LastUpdateCol + ">%27" + updated + "%27" // "%27" = "'"
+
+	err = readAndUpdate(request, db)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 type taxiTrip struct {
@@ -58,4 +79,32 @@ func openConnection() (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
+}
+
+func lastUpdate(db *sql.DB) string {
+	query := "SELECT MAX(" + LastUpdateCol + ") FROM " + Table + ";"
+	defaultDate := "1978-04-09 06:00:00"
+	var updated string
+	err := db.QueryRow(query).Scan(&updated)
+	if err != nil {
+		fmt.Println(err)
+		return "Error"
+	}
+	// this condition might end up being nil instead
+	if updated == "NULL" {
+		updated = defaultDate
+	}
+	// get into the SODA format
+	updated = strings.Replace(updated, " ", "T", 1)
+	return updated
+}
+
+func readAndUpdate(u string, db *sql.DB) error {
+	var err error
+
+	// read the JSON line by line
+
+	// update the database with each line's record
+
+	return err
 }
