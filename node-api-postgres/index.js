b4f4197cdf15d3f2cd575a148fd8c8512eafa0d3
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-12-02 15:58:17 -0800

    added endpoint routes for GET requests

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
+const db = require('./queries')
+
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
@@ -10,9 +12,9 @@ app.use(
   })
 )
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.get('/taxis', db.getTaxiTrips)
+app.get('/taxis/:pickupCA', db.getTaxiTripsByPickup)
+app.get('/taxis/:dropoffCA', db.getTaxiTripsByDropoff)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
