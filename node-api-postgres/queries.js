0b329e254e110322200f5b8e1c5b6b36a899f861
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-12-02 15:45:35 -0800

    added some GETs to the taxitrips REST API

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -7,3 +7,64 @@ const pool = new Pool({
   port: 5434,
 })
 
+// get all taxi trips
+const getTaxiTrips = (request, response) => {
+    let q = `
+    SELECT
+        trip_id,
+        trip_start_timestamp,
+        trip_end_timestamp,
+        pickup_community_area,
+        dropoff_community_area
+    FROM taxitrips
+    ORDER BY trip_start_timestamp ASC`
+    pool.query(q, (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+// get all taxi trips originating in a community area
+const getPickup = (request, response) => {
+    const pickup = parseInt(request.params.pickup)
+    let q = `
+    SELECT
+        trip_id,
+        trip_start_timestamp,
+        trip_end_timestamp,
+        pickup_community_area,
+        dropoff_community_area
+    FROM taxitrips
+    WHERE pickup_community_area = $1
+    ORDER BY trip_start_timestamp ASC`
+    pool.query(q, [pickup], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+// get all taxi trips dropping off in a community area
+const getDropoff = (request, response) => {
+    const dropoff = parseInt(request.params.dropoff)
+    let q = `
+    SELECT
+        trip_id,
+        trip_start_timestamp,
+        trip_end_timestamp,
+        pickup_community_area,
+        dropoff_community_area
+    FROM taxitrips
+    WHERE dropoff_community_area = $1
+    ORDER BY trip_start_timestamp ASC`
+    pool.query(q, [dropoff], (error, results) => {
+        if (error) {
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
